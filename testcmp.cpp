c3d1881d855e3b76413ba020472011efbfa44317
Simakov Alexey <20062003g@mail.ru>
2025-03-19 02:01:54 +0500

    initial commit

--- testcmp.cpp
+++ testcmp.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
-#include     "mycomplex.h"
+#include "mycomplex.h"
 
 using namespace std;
 
 int main()
-{//тестирование класса комплексных чисел
+{
+  //С‚РµСЃС‚РёСЂРѕРІР°РЅРёРµ РєР»Р°СЃСЃР° РєРѕРјРїР»РµРєСЃРЅС‹С… С‡РёСЃРµР»
 
-    Complex A,B(-4), C(23.0, 45.9);
-cout<<A <<","<< B<<","<<C <<endl;
+Complex A,B(-4), C(23.0, 45.9);
+
+cout<<A <<"A:"<< A<<", B:" <<B <<", C:"<< C << endl; 
 
 A=B+C;
 
-cout<<"A=B+C,A="<< A<<endl;
+cout<<"A=B+C,A="<< A <<endl;
 
 Complex M=B-C;
 
 cout<< "M = B - C, M = " << M << endl;
-cout<<"M*A="<<M*A<<endl;
 
-cout<<"M/4.45="<<M/4.45<<endl;
+Complex N=M*A;
 
+cout<<"N*A="<<N<<endl;
 
-    Complex D;
+Complex O =M/4.45;
 
-  	cout<<"Enter complex number D = "; cin >>D;
+cout<<"O=M/4.45="<< O <<endl;
 
- A+=C+D;
+Complex D;
+
+cout<<"Enter complex number D = "; cin >>D;
+
+A+=C+D;
+
 cout << "D = " << D << "\nA = " << A << endl;
 
 return 0;}
